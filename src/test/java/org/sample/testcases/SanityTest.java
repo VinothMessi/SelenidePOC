bd21434314e855ad3fc81eb042e63785e7e17437
Vinoth Murali <vinothone2one@gmail.com>
2023-02-07 13:51:45 +0530

    Added all dependancies

---
+++ src/test/java/org/sample/testcases/SanityTest.java
@@ -0,0 +1,10 @@
+package org.sample.testcases;
+
+import org.testng.annotations.Test;
+
+public class SanityTest {
+    @Test
+    public void sanity() {
+        System.out.println("Sanity Testing");
+    }
+}
